c314b436dc3f8be25517a67cfcb2571f205dae7a
howon <howonbyun@gmail.com>
2017-11-29 22:12:29 -0500

    sanity check for stitch.py

--- stitch.py
+++ stitch.py
@@ -3,19 +3,27 @@ import os
 import sys
 import numpy as np
 
-LEFT_DIR = argv[1]
-RIGHT_DIR = argv[2]
-OUTPUT_DIR = argv[3]
+def main():
+	if len(sys.argv) != 4:
+		print "Usage: python stitch.py <semantics_dir> <images_dir> <output_dir>"
+		sys.exit(1)
 
-semantics_paths = os.listdir()
-images_paths = os.listdir(RIGHT_DIR)
+	LEFT_DIR = sys.argv[1]
+	RIGHT_DIR = sys.argv[2]
+	OUTPUT_DIR = sys.argv[3]
 
-img_paths = zip(semantics_paths, images_paths, range(len(images_paths)))
+	semantics_paths = os.listdir()
+	images_paths = os.listdir(RIGHT_DIR)
 
-for x, y, i in img_paths:
-	left_image = imageio.imread(os.path.join(LEFT_DIR, x))
-	right_image = imageio.imread(os.path.join(RIGHT_DIR, y))
+	img_paths = zip(semantics_paths, images_paths, range(len(images_paths)))
 
-	imgs_comb = np.hstack(([left_image, right_image]))
-	out_path = os.path.join(OUTPUT_DIR, "{}.png".format(i))
-	imgs_comb_pic = imageio.imwrite(out_path, imgs_comb)
+	for x, y, i in img_paths:
+		left_image = imageio.imread(os.path.join(LEFT_DIR, x))
+		right_image = imageio.imread(os.path.join(RIGHT_DIR, y))
+
+		imgs_comb = np.hstack(([left_image, right_image]))
+		out_path = os.path.join(OUTPUT_DIR, "{}.png".format(i))
+		imgs_comb_pic = imageio.imwrite(out_path, imgs_comb)
+
+if __name__ == "__main__":
+	main()
